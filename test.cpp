9f38923a74bbfa0c8ac9398305f6f7e05e8c9fc7
Kuugo <renlin2002@163.com>
2026-03-25 14:26:09 +0800

    修复复试.md中的拼写错误，将tolewer更正为tolower

--- test.cpp
+++ test.cpp
@@ -3,19 +3,19 @@
 #include <numeric>
 #include<cmath>
 using namespace std;
-int main(){
+int main() {
 	vector<int> primes;
-	for(int num=2;num<=10000;num++){
-		bool is_prime=true;
-		for(int i=2;i*i<num;i++){
-			if(num%i==0){
-				is_prime=false;
+	for (int num = 2; num <= 10000; num++) {
+		bool is_prime = true;
+		for (int i = 2; i * i < num; i++) {
+			if (num % i == 0) {
+				is_prime = false;
 				break;
 			}
 		}
-		if(is_prime){
+		if (is_prime) {
 			primes.push_back(num);
 		}
-		
+
 	}
 }
